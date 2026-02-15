534f9558eb2fb27e832f400de0dc4f8b675811e9
DaxH <dari.xavi.bri@gmail.com>
2026-02-14 19:42:56 -0500

    config pages

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
 	return (
 		<>
 			<audio autoPlay loop>
-				<source src="/song/song.mp3" type="audio/mpeg" />
+				<source src="song/song.mp3" type="audio/mpeg" />
 			</audio>
 			<Dialog open={isOpenModal} onClose={handleCloseModal} fullWidth maxWidth="md">
 				<DialogContent>
@@ -26,7 +26,7 @@ const App = () => {
 						<Typography fontFamily="Lucy" variant="h2" align="center" sx={{ color: '#fca5a8' }}>¡Feliz día de San Valentín!</Typography>
 						<Typography fontFamily="Lucy" variant="h3" align="center" sx={{ color: '#fca5a8' }}>Cada día contigo es un regalo especial. Tu sonrisa ilumina mis días y tu presencia llena mi corazón de alegría. Eres alguien verdaderamente extraordinaria que ha transformado mi vida de manera hermosa.</Typography>
 						<Typography fontFamily="Lucy" variant="h3" align="center" sx={{ color: '#fca5a8' }}>¡Te amo infinitamente!</Typography>
-						<img src="/images/cats.png" width='400px' height='400px' alt="cats" />
+						<img src="images/cats.png" width='400px' height='400px' alt="cats" />
 					</Stack>
 				</DialogContent>
 				{[...Array(100)].map((_, i) => (
@@ -35,10 +35,10 @@ const App = () => {
 			</Dialog>
 			<div className={isClick ? "box open" : "box"}>
 				<div className={isClick ? 'top-box open' : "top-box close"} onClick={handleOpenModal}>
-					<img src="/images/top-box.svg" width='100%' height='100%' alt="top-box" />
+					<img src="images/top-box.svg" width='100%' height='100%' alt="top-box" />
 				</div>
 				<div className="bottom-box" onClick={handleOpenModal}>
-					<img src="/images/content-box.svg" width='100%' height='100%' alt="content-box" />
+					<img src="images/content-box.svg" width='100%' height='100%' alt="content-box" />
 				</div>
 			</div>
 			<div className="text">
